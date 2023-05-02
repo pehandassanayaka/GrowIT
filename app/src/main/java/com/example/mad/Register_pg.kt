fa0ea9a5e0273044b5d8d1505e5604dbc90ff45d
pehandassanayaka <pehandassanayaka@gmail.com>
2023-05-02 18:09:44 +0530

    Register page functions completed

--- app/src/main/java/com/example/mad/Register_pg.kt
+++ app/src/main/java/com/example/mad/Register_pg.kt
@@ -3,66 +3,82 @@ package com.example.mad
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.mad.databinding.ActivityRegisterPgBinding
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Register_pg : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRegisterPgBinding
-    private lateinit var firebaseAuth: FirebaseAuth
-
+     private var mFirebaseAuth = FirebaseAuth.getInstance()
+     private var firebaseStore = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_pg)
 
-        binding = ActivityRegisterPgBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val registration_btn = findViewById<TextView>(R.id.btnRegister)
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        registration_btn.setOnClickListener{
+            /**
+             * Changing the details to strings after being capturing
+             */
 
-        binding.goToLogin.setOnClickListener {
-            val intent = Intent(this, LoginNew::class.java)
-            startActivity(intent)
-        }
+            val name = findViewById<EditText>(R.id.inputName).text.toString()
+            val phone = findViewById<EditText>(R.id.inputMobile).text.toString()
+            val address = findViewById<EditText>(R.id.inputAddress).text.toString()
+            val email = findViewById<EditText>(R.id.inputEmail).text.toString()
+            val password = findViewById<EditText>(R.id.inputPassword).text.toString()
 
-        binding.btnRegister.setOnClickListener {
-            val name = binding.inputName.text.toString()
-            val phone = binding.inputMobile.text.toString()
-            val address = binding.inputAddress.text.toString()
-            val email = binding.inputEmail.text.toString()
-            val pass = binding.inputPassword.text.toString()
+            /**
+             * Creating a user onb firebase
+             */
 
-            if (name.isNotEmpty() && phone.isNotEmpty() && address.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty()) {
-                firebaseAuth.createUserWithEmailAndPassword(email, pass)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            val user = firebaseAuth.currentUser
-                            val uid = user?.uid
-                            val db = FirebaseDatabase.getInstance().reference
-                            db.child("users").child(uid!!).setValue(
-                                hashMapOf(
-                                    "name" to name,
-                                    "phone" to phone,
-                                    "address" to address,
-                                    "email" to email
-                                )
-                            ).addOnCompleteListener { task ->
-                                if (task.isSuccessful) {
-                                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
-                                    finish()
-                                } else {
-                                    Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                        } else {
-                            Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+             mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                 .addOnCompleteListener(
+                    OnCompleteListener {registrationProcess ->
+                        if(registrationProcess.isSuccessful){
+                            /**
+                             * if the user is create then we can store all the user details on t5he firebase database using a data class user.kt
+                             * The users will be stored under a collection called user. This is implemented using the function below
+                             * When the user is registered, gets a unique id and this will be stored too
+                             */
+                            val currentUserId = mFirebaseAuth.currentUser!!.uid
+                            val userDetailsToStore = User( currentUserId, name, phone, address, email )
+
+                            //Passing the userDetailsToStore object into the registerUser() function
+                            registerUser(userDetailsToStore)
                         }
                     }
-            } else {
-                Toast.makeText(this, "Empty Fields are not allowed", Toast.LENGTH_SHORT).show()
-            }
+                 )
 
         }
+
+
+
+    }
+
+    fun registerUser(userData:User){
+        firebaseStore.collection("users") // users is the name of the collection that will be created in firebase
+            .document()
+            // Using the fields created in the  class with help of UserInfo
+            .set(userData).addOnSuccessListener {
+                // Displaying a success message to indicate that registration is successful
+
+                Toast.makeText(this,"Registered Successfully",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            // If the registration fails, Display a message
+            .addOnFailureListener { e ->
+                Log.e( javaClass.simpleName,
+                    "Error while registering.",
+                    e
+                )
+            }
     }
 }
---
+++ app/src/main/java/com/example/mad/User.kt
@@ -0,0 +1,12 @@
+package com.example.mad
+
+import android.location.Address
+
+data class User(
+    val user_id:String = "",
+    val name:String = "",
+    val phone:String = "",
+    val address:String = "",
+    val email:String = "",
+
+)
