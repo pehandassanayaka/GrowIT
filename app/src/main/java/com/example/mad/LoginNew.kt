3a1eff2b4b1ab704d7268cf563aebe59940c2925
pehandassanayaka <pehandassanayaka@gmail.com>
2023-05-02 12:06:51 +0530

    Register page main function works but other data passing not work

--- app/src/main/java/com/example/mad/LoginNew.kt
+++ app/src/main/java/com/example/mad/LoginNew.kt
@@ -1,11 +1,47 @@
 package com.example.mad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mad.databinding.ActivityLoginNewBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class LoginNew : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginNewBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_new)
+        binding = ActivityLoginNewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.goToRegister.setOnClickListener{
+            val intent = Intent(this, Register_pg::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener{
+            val email = binding.loginInputEmail.text.toString()
+            val pass = binding.inputPassword.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent = Intent(this, UserProfile::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                    }
+            }
+            else{
+                Toast.makeText(this, "Empty Fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mad/Register_pg.kt
+++ app/src/main/java/com/example/mad/Register_pg.kt
@@ -1,11 +1,68 @@
 package com.example.mad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mad.databinding.ActivityRegisterPgBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class Register_pg : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterPgBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_pg)
+
+        binding = ActivityRegisterPgBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.goToLogin.setOnClickListener {
+            val intent = Intent(this, LoginNew::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val name = binding.inputName.text.toString()
+            val phone = binding.inputMobile.text.toString()
+            val address = binding.inputAddress.text.toString()
+            val email = binding.inputEmail.text.toString()
+            val pass = binding.inputPassword.text.toString()
+
+            if (name.isNotEmpty() && phone.isNotEmpty() && address.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val user = firebaseAuth.currentUser
+                            val uid = user?.uid
+                            val db = FirebaseDatabase.getInstance().reference
+                            db.child("users").child(uid!!).setValue(
+                                hashMapOf(
+                                    "name" to name,
+                                    "phone" to phone,
+                                    "address" to address,
+                                    "email" to email
+                                )
+                            ).addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                                    finish()
+                                } else {
+                                    Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        } else {
+                            Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Empty Fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
